c223140d5b6375aa1bc6b0a7095eeb6899724ff3
Kayla Barton <kdbarton1s@gmail.com>
2019-09-22 13:04:38 -0500

    Added average stars

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -22,4 +22,8 @@ class Movie < ApplicationRecord
   def self.released
     where("released_on < ?", Time.now).order("released_on desc")
   end
+
+  def average_stars
+    reviews.average(:stars)
+  end
 end
